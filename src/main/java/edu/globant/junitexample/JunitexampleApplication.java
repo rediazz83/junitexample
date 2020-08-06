2db94253aed7e9d218238f6708a2103f1b84b150
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-05 21:06:21 -0400

    Codigo base

---
+++ src/main/java/edu/globant/junitexample/JunitexampleApplication.java
@@ -0,0 +1,13 @@
+package edu.globant.junitexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitexampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/globant/junitexample/JunitexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.globant.junitexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
