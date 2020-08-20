af8a10f5691e1f487d896dcf4f36ce27c8e451dc
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-19 22:21:40 -0400

    Display name (better description) example

--- src/test/java/edu/globant/junitexample/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/edu/globant/junitexample/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package edu.globant.junitexample.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,6 +15,7 @@ class IndexControllerTest {
         controller = new IndexController();
     }
 
+    @DisplayName("Test proper view name is returned for index page")
     @Test
     void index() {
         assertEquals("index", controller.index(), "Wrong view returned");
