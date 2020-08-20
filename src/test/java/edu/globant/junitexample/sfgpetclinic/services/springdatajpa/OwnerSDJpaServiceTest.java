8d1e5e7a0235d702afd582771a61902b4e8fae8d
Rodrigo Diaz <esteban.diaz@globant.com>
2020-08-19 22:16:54 -0400

    Skipping unit tests example

---
+++ src/test/java/edu/globant/junitexample/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,47 @@
+package edu.globant.junitexample.sfgpetclinic.services.springdatajpa;
+
+import edu.globant.junitexample.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled(value = "Disabled until we learn mocking")
+class OwnerSDJpaServiceTest {
+
+    private OwnerSDJpaService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new OwnerSDJpaService(null, null, null);
+    }
+
+    @Disabled
+    @Test
+    void findByLastName() {
+        Owner foundOwner = service.findByLastName("Buck");
+    }
+
+    @Test
+    void findAllByLastNameLike() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
